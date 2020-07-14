79a3f1befc18f3be90c4b557e96adf617eb559fc
Gilbert Eaton <gilberteaton@gmail.com>
2020-07-14 18:16:16 +1000

    multi-page version

--- DashBoard/firebase-dashboard/src/App.vue
+++ DashBoard/firebase-dashboard/src/App.vue
@@ -1,85 +1,23 @@
+
 <template>
   <div id="app">
-    <!-- <img alt="pano" src="./assets/pano_small.jpg"/> -->
-    <First title="Strawberry Dashboard"/>
-    <!-- <First msg="First Component"/> -->
-    <h2>Strawberry Defects by Month</h2>
-    <vue-frappe
-      id="defects"
-      title="Number of defects by month"
-      type="line"
-      :labels="['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']"
-      :height="500"
-      :colors="['#008F68', '#FAE042', '#32a852', '', '']"
-      :lineOptions="{regionFill: 1}"
-      :dataSets="this.defectData">
-    </vue-frappe>
-
-    <h2>Total Breakdown</h2>
-
-    <vue-frappe
-      id="breakdown"
-      title="Total Breakdown"
-      type="pie"
-      :labels="['Underripe', 'Overripe', 'Bruise', 'Foreign', 'Small']"
-      :height="500"
-      :colors="['#008F68', '#FAE042', '#32a852', '', '']"
-      :dataSets="this.breakdownData">
-    </vue-frappe>
-
-
+    <h1>Strawberry Dashboard</h1>
+    <nav>
+        <router-link to="/"> Home </router-link>
+        <router-link to="/bar"> | Bar Chart </router-link>
+        <router-link to="/pie">  | Pie Chart </router-link>
+    </nav>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import First from './components/First.vue'
-
-var defectType = 'bar'
-var breakdownType = 'pie'
-
 export default {
-  name: 'App',
-  data() {
-    return {
-      defectData: [{
-                  name: "Underripe", chartType: defectType,
-                  values: [40, 275, 370, 455, 572, 580, 585, 387, 288, 90, 2, 0]
-              },
-              {
-                  name: "Overripe", chartType: defectType,
-                  values: [8, 85, 91, 89, 85, 190, 297, 410, 412, 321, 30, 5]
-              },
-              {
-                  name: "Bruise", chartType: defectType,
-                  values: [0, 5, 291, 289, 285, 290, 297, 310, 312, 321, 33, 4]
-              },
-              {
-                  name: "Foreign", chartType: defectType,
-                  values: [0, 15, 51, 89, 85, 90, 97, 50, 12, 1, 0, 0]
-              },
-              {
-                  name: "Small", chartType: defectType,
-                  values: [80, 85, 91, 189, 385, 290, 97, 61, 99, 21, 30, 5]
-              }
-            ],
-
-            breakdownData: [{
-                        name: "Defects", chartType: breakdownType,
-                        values: [3644, 2023, 2437, 490, 1433]
-                    }
-                  ]
-
-    }
-  },
-  components: {
-    First
-  }
 }
-
-
-
 </script>
+
 
 <style>
 #app {
--- DashBoard/firebase-dashboard/src/main.js
+++ DashBoard/firebase-dashboard/src/main.js
@@ -1,12 +1,42 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 // import VueFrappe from 'vue2-frappe';
 import Chart from 'vue2-frappe'
 import App from './App.vue'
+import Pie from './components/Pie.vue'
+import Bar from './components/Bar.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Chart)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+//Routes
+// const Home  = {
+//   template: '<div>Home</div>'
+// }
+// const Bar  = {
+//   template: '<div>Bar</div>'
+// }
+// const Pie  = {
+//   template: '<div>Pie</div>'
+// }
+const HomePage = Home
+const BarChart = Bar
+const PieChart = Pie
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/bar', component: BarChart },
+  { path: '/pie', component: PieChart }
+]
+const router = new VueRouter({
+  routes
+})
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
